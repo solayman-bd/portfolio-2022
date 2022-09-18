a18f4bd883bed472db219b370b89ee1a14fd3ac3
solayman-bd <mdsolaymannuhin@gmail.com>
2022-09-18 20:13:19 +0600

    Hero section integrated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import Image from "next/image";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
+import BackgroundCircle from "./BackgroundCircle";
+import profileImage from "../public/assets/images/profile.jpg";
+import Link from "next/link";
 type Props = {};
 
 const Hero: React.FC = (props: Props) => {
@@ -7,15 +11,42 @@ const Hero: React.FC = (props: Props) => {
     words: [
       "Hi, Myself Solayman",
       "A Programming Enthusiast ",
-      "A MERN stack developer",
+      "A MERN stack developer....",
     ],
     loop: true,
     delaySpeed: 200,
   });
   return (
-    <div>
-      <span>{text}</span>
-      <Cursor cursorColor="#F7AB0A" />
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircle />
+      <Image
+        src={profileImage}
+        className="rounded-full h-32 w-32 object-cover"
+      />
+
+      <div className="z-10">
+        <h2 className="text-sm uppercase text-gray-600 tracking-[0.8rem]">
+          an Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span>{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="hero-button">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="hero-button">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="hero-button">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="hero-button">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
